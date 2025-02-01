637f3f28ae6b1d7d4beedc547d67676416a31a49
umm e habiba <128902333+Umm-e-Habiba-405@users.noreply.github.com>
2025-02-01 22:24:45 +0500

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images:{
+    domains:['cdn.sanity.io'],
+  },
+  
+};
+
+export default nextConfig;
